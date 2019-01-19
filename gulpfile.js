a6e94889cbd3edd9609cb5f942656c05a52ca411
Carlos Escriche <escrichov@gmail.com>
2019-01-19 16:13:04 +0100

    Updated gulpfile to latest

--- gulpfile.js
+++ gulpfile.js
@@ -35,82 +35,74 @@ const destJavascriptFiles = path.join(destDirectory, "/js/**/*.js");
 
 
 // Clean all files in destDirectory
-gulp.task('clean', function () {
-    return gulp.src(destDirectory, {read: false})
+function cleanTask (cb) {
+    return gulp.src(destDirectory, {read: false, allowEmpty: true})
         .pipe(clean());
-});
+}
+
+// Copy Javascript files in sourceStaticDirectory to destStaticDirectory
+function javascriptCopyFiles (cb) {
+    return gulp.src(sourceJavascriptFiles)
+        .pipe(gulp.dest(destJavascriptDirectory));
+}
+
+// Copy Javascript Vendor files in sourceStaticDirectory to destStaticDirectory
+function javascriptCopyVendor (cb) {
+    return gulp.src(sourceNpmJavascriptFiles)
+        .pipe(gulp.dest(destJavascriptVendorDirectory));
+}
 
 // Copy Static files in sourceStaticDirectory to destStaticDirectory
-gulp.task('javascript', function () {
-     return gulp.src(sourceJavascriptFiles)
-             .pipe(gulp.dest(destJavascriptDirectory));
-});
-
-// Copy Static files in sourceStaticDirectory to destStaticDirectory
-gulp.task('javascript:vendor', function () {
-     return gulp.src(sourceNpmJavascriptFiles)
-             .pipe(gulp.dest(destJavascriptVendorDirectory));
-});
-
-// Copy Static files in sourceStaticDirectory to destStaticDirectory
-gulp.task('static', function () {
-     return gulp
-             .src(sourceStaticFiles)
-             .pipe(gulp.dest(destStaticDirectory));
-});
+function staticCopy (cb) {
+    return gulp
+        .src(sourceStaticFiles)
+        .pipe(gulp.dest(destStaticDirectory));
+}
 
 // Sass
-gulp.task('sass', function () {
-  return gulp.src(sourceSassFiles)
-    .pipe(sourcemaps.init())
-    .pipe(sass({ style: 'expanded', sourceComments: 'map', errLogToConsole: true }))
-    .on('error', sass.logError)
-    .pipe(sourcemaps.write())
-    .pipe(autoprefixer('last 2 version', "> 1%", 'ie 8', 'ie 9'))
-    .pipe(gulp.dest(destCssDirectory))
-    .pipe(browserSync.stream());
-});
+function sassBuild (cb) {
+    return gulp.src(sourceSassFiles)
+        .pipe(sourcemaps.init())
+        .pipe(sass({ style: 'expanded', sourceComments: 'map', errLogToConsole: true }))
+        .on('error', sass.logError)
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer('last 2 version', "> 1%", 'ie 8', 'ie 9'))
+        .pipe(gulp.dest(destCssDirectory))
+        .pipe(browserSync.stream());
+}
 
 // Compile views in Nunjucks Format
-gulp.task('views', function () {
-  return gulp.src(sourceViewsFiles)
-    .pipe(nunjucksRender({
-      path: ['views']
-    }))
-    .on('error', gutil.log)
-    .pipe(gulp.dest(destHtmlDirectory))
-    .pipe(browserSync.stream())
-});
+function viewsBuild (cb) {
+    return gulp.src(sourceViewsFiles)
+        .pipe(nunjucksRender({
+          path: ['views']
+        }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest(destHtmlDirectory))
+        .pipe(browserSync.stream())
+}
 
-// Browser Sync
-gulp.task('browserSync', ['static', 'sass', 'views', 'javascript', 'javascript:vendor'], function() {
+// // Browser Sync
+function watchAll (cb) {
 
   browserSync.init({
     server: destDirectory
   });
-  
-  gulp.watch(sourceSassFiles, ['sass']);
-  gulp.watch(sourceViewsFiles, ['views']);
-  gulp.watch(sourceJavascriptFiles, ['javascript']);
-  gulp.watch(sourceNpmJavascriptFiles, ['javascript:vendor']);
+
+  gulp.watch(sourceSassFiles, sassBuild);
+  gulp.watch(sourceViewsFiles, viewsBuild);
+  gulp.watch(sourceJavascriptFiles, javascriptCopyFiles);
+  gulp.watch(sourceNpmJavascriptFiles, javascriptCopyVendor);
 
   gulp.watch(destHtmlFiles).on('change', browserSync.reload);
   gulp.watch(destJavascriptFiles).on('change', browserSync.reload);
-});
+}
 
-// Build files
-gulp.task('build', ['clean'], function() {
-  gulp.start('static'); 
-  gulp.start('sass');
-  gulp.start('views');
-  gulp.start('javascript');
-  gulp.start('javascript:vendor');
-});
+const buildTask = gulp.series(cleanTask, gulp.parallel(staticCopy, sassBuild, viewsBuild, javascriptCopyFiles, javascriptCopyVendor))
+const serveTask = gulp.series(buildTask, watchAll)
 
-// Clean Build + Static files with Browser Sync
-gulp.task('serve', ['clean'], function() {
-  gulp.start('browserSync'); 
-});
-
+exports.build = buildTask
+exports.serve = serveTask
+exports.clean = cleanTask
 // Default Task
-gulp.task('default', ['serve']);
+exports.default = serveTask
